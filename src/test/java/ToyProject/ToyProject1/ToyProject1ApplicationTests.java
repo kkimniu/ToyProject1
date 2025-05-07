98e9e0bf9bb7bf2885713b1e0217ef537e27b94f
Kim Dong Min <kkimniu@gmail.com>
2025-05-08 00:47:09 +0900

    초기 커밋

---
+++ src/test/java/ToyProject/ToyProject1/ToyProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ToyProject.ToyProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
