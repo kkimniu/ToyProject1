f34680b69450752ec6bba92c0f438491111278e1
Kim Dong Min <kkimniu@gmail.com>
2025-05-13 09:07:20 +0900

    jpa엔티티 추가

---
+++ src/main/java/ToyProject/ToyProject1/domain/member.java
@@ -0,0 +1,85 @@
+package ToyProject.ToyProject1.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+    private String id;
+    private String pwd ;
+    private String name;
+    private String email;
+    private String tel;
+    private String addr;
+    private LocalDateTime joindate;
+
+    public int getIdx() {
+        return idx;
+    }
+
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public LocalDateTime getJoindate() {
+        return joindate;
+    }
+
+    public void setJoindate(LocalDateTime joindate) {
+        this.joindate = joindate;
+    }
+}
