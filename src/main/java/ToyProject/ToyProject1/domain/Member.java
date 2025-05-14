eef78f9a569546d3270a91491983b28acdeeaf45
Kim Dong Min <kkimniu@gmail.com>
2025-05-14 11:08:01 +0900

    Modify 엔티티에 변수명 수정

--- src/main/java/ToyProject/ToyProject1/domain/Member.java
+++ src/main/java/ToyProject/ToyProject1/domain/Member.java
@@ -1,38 +1,38 @@
 package ToyProject.ToyProject1.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idx;
-    private String id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)//시퀀스처럼 사용하기위해 선언
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String userid;
+    @Column(nullable = false)
     private String pwd ;
     private String name;
     private String email;
     private String tel;
     private String addr;
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")//값을 저장할때 현재 시간을 저장함
     private LocalDateTime joindate;
 
-    public int getIdx() {
-        return idx;
-    }
-
-    public void setIdx(int idx) {
-        this.idx = idx;
-    }
-
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
     }
 
     public String getPwd() {
