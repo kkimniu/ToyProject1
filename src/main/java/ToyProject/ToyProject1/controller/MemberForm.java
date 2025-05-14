1d39cbec7e1095af7bb0ccfad72cc40ed12f54e9
Kim Dong Min <kkimniu@gmail.com>
2025-05-14 11:09:22 +0900

    Add MemberForm 추가

---
+++ src/main/java/ToyProject/ToyProject1/controller/MemberForm.java
@@ -0,0 +1,62 @@
+package ToyProject.ToyProject1.controller;
+
+import jakarta.persistence.Column;
+
+import java.time.LocalDateTime;
+
+public class MemberForm {
+    private String userid;
+    private String pwd ;
+    private String name;
+    private String email;
+    private String tel;
+    private String addr;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
